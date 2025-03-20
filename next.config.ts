98a2dc8ebfabe7c9190516274ef3597b611ca3e9
markpausanos <work.markpausanos@gmail.com>
2025-03-21 04:37:36 +0800

    Add Ignore during builds for eslint

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,18 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	async redirects() {
-		return [
-			{
-				source: '/',
-				destination: '/home',
-				permanent: true,
-			},
-		];
-	},
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/home",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
